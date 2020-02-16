48b8f955bfee4078bac55afdb7f8e098fb2fdd22
Ravisher Singh <ravishersingh@Ravishers-MacBook-Air.local>
2020-02-16 11:19:52 -0700

    First Commit

---
+++ SinghRavisherPA1_outline.cpp
@@ -0,0 +1,70 @@
+/*Name: Ravisher Singh
+Last Date Modified:
+Assignment: pseudo code
+Description:
+Status: Compiled and ran successfully
+Met all requirements
+*/
+//Declare a variable choice as int
+//Start an infinite loop until the user chooses exit
+      //Displaying name with cout
+      //Displaying ID with cout
+      // Displaying “Almost always 99” with cout
+      //Displaying “Always 3” with cout
+      //Displaying “Exit” with cout
+      //Displaying “Always 3: Read from File” with cout
+      // Get/read user input by using cin
+  //Declare variables for case 1,
+  //Declare variables for case2,
+  //Declare variable for case 4 char fileName[25]
+// Use switch statement for case1
+    //if number is less than 10
+    // multiply by 10  it becomes num1
+//if num1 is double digit from 10-99
+    // separate tens and ones
+//Reverse the digits (ones * 10) + (tens/10); // finding reverse it becomes num2
+    //Subtract the  num2 from the num1 it becomes num3
+//If the num3  is negative, multiply by -1.
+    // If the num3 is less than 10, multiply it by 10.
+    // Take the num3, reverse the digits it becomes num4
+    // Add num3 to num4
+//Cout final output
+   //if num4 =99 then “Congrats the final number is 99”
+   //else “ I must have done some wrong calculations”
+               //break
+//Use switch statement for case2
+     // get the user to enter number with cout
+      // read integer with cin
+    //Perform required operations
+      // Double the number *2 it becomes number1
+      // add nine to number1 it becomes number2
+      // Subtract 3 to number2 it becomes number3
+     //Divide number3 by 2 it becomes number4
+      // Subtract number4-number
+  //cout final statement if number4 =3, “Congrats! The final number is 3”
+  //else cout “I must have done some wrong calculations"
+               //break
+//Use switch statement for case 3
+        //if user presses 3 cout “ Exiting the program”
+
+//Use switch statement for case 4
+       // cout “Name of the file”
+       // user “fptr.open(fileName);
+       //if file name of file does not match cout “File does not exist!”
+       //use else statement read file line by line
+       //print read integer from file
+       // Use same operations as case 2
+       // get the user to enter number with cout
+       // read integer with cin
+       //Perform required operations
+      // Double the number *2 it becomes number1
+      // add nine to number1 it becomes number2
+      // Subtract 3 to number2 it becomes number3
+         //Divide number3 by 2 it becomes number4
+    // Subtract number4-number
+        //cout final statement if number4 =3, “Congrats! The final number is 3”
+      //else cout “I must have done some wrong calculations"
+                //break
+       //default
+    //break (end of switch statement)
+       // if choice is 3 use break statement to end while loop
